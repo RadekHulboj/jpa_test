20349c180213cd4416c031401febe407f602884d
Hulboj <radoslaw.hulboj@capgemini.com>
2020-11-19 13:56:23 +0100

    [jaxbPrototype] - the simple prototype done

---
+++ src/main/java/poc/business/Marshalling.java
@@ -0,0 +1,39 @@
+package poc.business;
+
+import poc.model.Book;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+
+public class Marshalling {
+
+    public static void main (String[] args) throws JAXBException, IOException {
+        marshal();
+        Book unmarshall = unmarshall();
+    }
+
+    public static void marshal() throws JAXBException, IOException {
+        Book book = new Book();
+        book.setId(1L);
+        book.setName("Book1");
+        book.setAuthor("Author1");
+        book.setDate(new Date());
+
+        JAXBContext context = JAXBContext.newInstance(Book.class);
+        Marshaller mar= context.createMarshaller();
+        mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        mar.marshal(book, new File("./book.xml"));
+    }
+
+    public static Book unmarshall() throws JAXBException, IOException {
+        JAXBContext context = JAXBContext.newInstance(Book.class);
+        return (Book) context.createUnmarshaller()
+                .unmarshal(new FileReader("./book.xml"));
+    }
+
+}
