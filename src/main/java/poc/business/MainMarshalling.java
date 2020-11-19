df403a050e0f6225a27b7bd1d08191f4e778470a
Hulboj <radoslaw.hulboj@capgemini.com>
2020-11-19 15:47:43 +0100

    [xsdToJava] - unmarshaling is workig for MZ01Meldung type

--- src/main/java/poc/business/MainMarshalling.java
+++ src/main/java/poc/business/MainMarshalling.java
@@ -1,20 +1,23 @@
 package poc.business;
 
+import de.drv_bund.zfa.rebsy.rentenbezugsmitteilung.MZ01Meldung;
 import poc.model.Book;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
 
-public class Marshalling {
+public class MainMarshalling {
 
     public static void main (String[] args) throws JAXBException, IOException {
         marshal();
         Book unmarshall = unmarshall();
+        MZ01Meldung mz01Meldung = unmarshallEzvk();
     }
 
     public static void marshal() throws JAXBException {
@@ -34,5 +37,25 @@ public class Marshalling {
         JAXBContext context = JAXBContext.newInstance(Book.class);
         return (Book) context.createUnmarshaller()
                 .unmarshal(new FileReader("./src/main/resources/book.xml"));
+    }
+
+
+    public static MZ01Meldung unmarshallEzvk() {
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(MZ01Meldung.class);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        MZ01Meldung unmarshal = null;
+        try {
+            unmarshal = (MZ01Meldung) context.createUnmarshaller()
+                    .unmarshal(new FileReader("./src/main/resources/anonymization.xml"));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return unmarshal;
     }
 }
